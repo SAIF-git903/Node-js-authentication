e7d0d7a9d536df374ff1124f9955517fa3686caf
Saif Ali <saif@gmmail.com>
2023-05-23 15:11:30 +0500

    completed basic user auth with node js

--- server.js
+++ server.js
@@ -14,14 +14,29 @@ app.post("/users", async (req, res) => {
   try {
     const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
-    console.log(salt);
-    console.log(hashedPassword);
 
     const user = { password: hashedPassword, name: req.body.name };
     users.push(user);
     res.status(201).send();
   } catch (err) {
     res.status(500).send(err);
+  }
+});
+
+app.post("/users/login", async (req, res) => {
+  const user = await users.find(user => user.name === req.body.name);
+  if (user === null) {
+    return res.status(400).send("Cannot find User");
+  }
+
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.send("Success");
+    } else {
+      res.send("Not allowed");
+    }
+  } catch {
+    res.status(500).send();
   }
 });
 
